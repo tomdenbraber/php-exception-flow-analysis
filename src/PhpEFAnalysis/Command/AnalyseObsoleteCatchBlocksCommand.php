ff56ce21d8327ef4f4fc3b6f555e005c0df39890
Tom den Braber <tomdenbraber@gmail.com>
2017-06-21 17:06:00 +0200

    output specific instances for try/catch block analysis, and make special case for 'unknown' exceptions

--- src/PhpEFAnalysis/Command/AnalyseObsoleteCatchBlocksCommand.php
+++ src/PhpEFAnalysis/Command/AnalyseObsoleteCatchBlocksCommand.php
@@ -41,33 +41,43 @@ class AnalyseObsoleteCatchBlocksCommand extends Command {
 		}
 		$ch = json_decode(file_get_contents($class_hierarchy_file), $assoc = true);
 
-		$catch_block_count = [
-			"type not encountered" => 0,
-			"type caught by earlier catch" => 0,
-			"(sub)type caught" => 0,
-		];
+		$catch_blocks["type not encountered"] = [];
+		$catch_blocks["type not encountered, but unknown encountered"] = [];
+		$catch_blocks["type caught by earlier catch"] = [];
+		$catch_blocks["(sub)type caught"] = [];
 
 		foreach ($ef as $scope_name => $scope) {
-			$current_scope_count = $this->analyseScope($scope, $ch);
+			$current_scope_catch_blocks = $this->analyseScope($scope, $ch);
 
-			$catch_block_count["type not encountered"] += $current_scope_count["type not encountered"];
-			$catch_block_count["type caught by earlier catch"] += $current_scope_count["type caught by earlier catch"];
-			$catch_block_count["(sub)type caught"] += $current_scope_count["(sub)type caught"];
+			$catch_blocks["type not encountered"] = array_merge($catch_blocks["type not encountered"], $current_scope_catch_blocks["type not encountered"]);
+			$catch_blocks["type not encountered, but unknown encountered"] = array_merge($catch_blocks["type not encountered, but unknown encountered"], $current_scope_catch_blocks["type not encountered, but unknown encountered"]);
+			$catch_blocks["type caught by earlier catch"] = array_merge($catch_blocks["type caught by earlier catch"], $current_scope_catch_blocks["type caught by earlier catch"]);
+			$catch_blocks["(sub)type caught"] = array_merge($catch_blocks["(sub)type caught"], $current_scope_catch_blocks["(sub)type caught"]);
 		}
+
+		$catch_blocks = array_merge($catch_blocks,
+			[
+				"type not encountered count" => count($catch_blocks["type not encountered"]),
+				"type not encountered, but unknown encountered count" => count($catch_blocks["type not encountered, but unknown encountered"]),
+				"type caught by earlier catch count" => count($catch_blocks["type caught by earlier catch"]),
+				"(sub)type caught count" => count($catch_blocks["(sub)type caught"]),
+			]
+		);
 
 		if (file_exists($output_path . "/obsolete-catch-blocks.json") === true) {
 			die($output_path . "/obsolete-catch-blocks.json already exists");
 		} else {
-			file_put_contents($output_path . "/obsolete-catch-blocks.json", json_encode($catch_block_count, JSON_PRETTY_PRINT));
+			file_put_contents($output_path . "/obsolete-catch-blocks.json", json_encode($catch_blocks, JSON_PRETTY_PRINT));
 			$output->write(json_encode(["obsolete catch blocks" => $output_path . "/obsolete-catch-blocks.json"]));
 		}
 	}
 
 	private function analyseScope($scope_data, $class_hierarchy) {
-		$catch_block_count = [
-			"type not encountered" => 0,
-			"type caught by earlier catch" => 0,
-			"(sub)type caught" => 0,
+		$catch_blocks = [
+			"type not encountered" => [],
+			"type not encountered, but unknown encountered" => [],
+			"type caught by earlier catch" => [],
+			"(sub)type caught" => [],
 		];
 
 
@@ -77,41 +87,56 @@ class AnalyseObsoleteCatchBlocksCommand extends Command {
 
 			foreach ($guarded_scope_data["catch clauses"] as $type => $caught_types) {
 				$catch_clause_type = strtolower($type);
+				$catch_clause_name = sprintf("%s<%s>", $guarded_scope_name, $catch_clause_type);
 				if (empty($caught_types) === false) {
-					$catch_block_count['(sub)type caught'] += 1;
+					$catch_blocks['(sub)type caught'][] = $catch_clause_name;
 				} else {
 					$catchable_types = $class_hierarchy["class resolved by"][$catch_clause_type];
 					$inclosed_encounters = $this->getEncountersForScope($guarded_scope_data["inclosed"][$inclosed_scope_name]);
 
 					$exception_already_caught = false;
+					$unknown_or_empty_encountered = false;
 
 					foreach ($inclosed_encounters as $inclosed_encounter) {
+						
 						if (isset($catchable_types[$inclosed_encounter]) === true) {
 							//the inclosed encounter *could* have been caught by this catch clause, but the catch clause itself is empty,
 							//meaning that inclosed_encounter has already been caught.
 							$exception_already_caught = true;
+						} else if ($inclosed_encounter === "" || $inclosed_encounter === "unknown") {
+							$unknown_or_empty_encountered = true;
 						}
 					}
 
 					if ($exception_already_caught === true) {
-						$catch_block_count["type caught by earlier catch"] += 1;
+						$catch_blocks["type caught by earlier catch"][] = $catch_clause_name;
+					} else if ($unknown_or_empty_encountered === true) {
+						$catch_blocks["type not encountered, but unknown encountered"][] = $catch_clause_name;
 					} else {
-						$catch_block_count["type not encountered"] += 1;
+						$catch_blocks["type not encountered"][] = $catch_clause_name;
 					}
 				}
 			}
 
-			$nested_scope_accumulated_counts = $this->analyseScope($guarded_scope_data["inclosed"][$inclosed_scope_name], $class_hierarchy);
+			$nested_scope_catch_blocks = $this->analyseScope($guarded_scope_data["inclosed"][$inclosed_scope_name], $class_hierarchy);
 
-			$catch_block_count["type not encountered"] += $nested_scope_accumulated_counts["type not encountered"];
-			$catch_block_count["type caught by earlier catch"] += $nested_scope_accumulated_counts["type caught by earlier catch"];
-			$catch_block_count["(sub)type caught"] += $nested_scope_accumulated_counts["(sub)type caught"];
+			$catch_blocks["type not encountered"] = array_merge($catch_blocks["type not encountered"], $nested_scope_catch_blocks["type not encountered"]);
+			$catch_blocks["type not encountered, but unknown encountered"] = array_merge($catch_blocks["type not encountered, but unknown encountered"], $nested_scope_catch_blocks["type not encountered, but unknown encountered"]);
+			$catch_blocks["type caught by earlier catch"] = array_merge($catch_blocks["type caught by earlier catch"], $nested_scope_catch_blocks["type caught by earlier catch"]);
+			$catch_blocks["(sub)type caught"] = array_merge($catch_blocks["(sub)type caught"], $nested_scope_catch_blocks["(sub)type caught"]);
 		}
 
-		return $catch_block_count;
+		return $catch_blocks;
 	}
 
 	private function getEncountersForScope($scope) {
-		return array_merge($scope["raises"], $scope["propagates"], $scope["uncaught"]);
+		$encounters = $scope["raises"];
+		foreach ($scope["propagates"] as $propagated_exceptions) {
+			$encounters = array_merge($propagated_exceptions, $encounters);
+		}
+		foreach ($scope["uncaught"] as $uncaught_exceptions) {
+			$encounters = array_merge($uncaught_exceptions, $encounters);
+		}
+		return $encounters;
 	}
 }
--- src/PhpEFAnalysis/Command/AnalyseObsoleteTryBlocksCommand.php
+++ src/PhpEFAnalysis/Command/AnalyseObsoleteTryBlocksCommand.php
@@ -30,56 +30,78 @@ class AnalyseObsoleteTryBlocksCommand extends Command {
 		}
 		$ef = json_decode(file_get_contents($exception_flow_file), $assoc = true);
 
-		$try_block_count = [
-			"encountered but nothing caught" => 0,
-			"nothing encountered" => 0,
-			"caught and encountered" => 0
+		$try_blocks = [
+			"encountered but nothing caught" => [],
+			"nothing caught, unknown encountered" => [],
+			"nothing encountered" => [],
+			"caught and encountered" => []
 		];
 
 		foreach ($ef as $scope => $scope_data) {
-			$scope_accumulated_counts = $this->analyseScope($scope_data);
-			$try_block_count["encountered but nothing caught"] += $scope_accumulated_counts["encountered but nothing caught"];
-			$try_block_count["nothing encountered"] += $scope_accumulated_counts["nothing encountered"];
-			$try_block_count["caught and encountered"] += $scope_accumulated_counts["caught and encountered"];
+			$scope_accumulated = $this->analyseScope($scope_data);
+			$try_blocks["encountered but nothing caught"] = array_merge($try_blocks["encountered but nothing caught"], $scope_accumulated["encountered but nothing caught"]);
+			$try_blocks["nothing caught, unknown encountered"] = array_merge($try_blocks["nothing caught, unknown encountered"], $scope_accumulated["nothing caught, unknown encountered"]);
+			$try_blocks["nothing encountered"] = array_merge($try_blocks["nothing encountered"], $scope_accumulated["nothing encountered"]);
+			$try_blocks["caught and encountered"] = array_merge($try_blocks["caught and encountered"], $scope_accumulated["caught and encountered"]);
 		}
+
+
+		$try_blocks["encountered but nothing caught count"] = count($try_blocks["encountered but nothing caught"]);
+		$try_blocks["nothing caught, unknown encountered count"] = count($try_blocks["nothing caught, unknown encountered"]);
+		$try_blocks["nothing encountered count"] = count($try_blocks["nothing encountered"]);
+		$try_blocks["caught and encountered count"] = count($try_blocks["caught and encountered"]);
 
 		if (file_exists($output_path . "/obsolete-try-blocks.json") === true) {
 			die($output_path . "/obsolete-try-blocks.json already exists");
 		} else {
-			file_put_contents($output_path . "/obsolete-try-blocks.json", json_encode($try_block_count, JSON_PRETTY_PRINT));
+			file_put_contents($output_path . "/obsolete-try-blocks.json", json_encode($try_blocks, JSON_PRETTY_PRINT));
 			$output->write(json_encode(["obsolete try blocks" => $output_path . "/obsolete-try-blocks.json"]));
 		}
 	}
 
 	private function analyseScope($scope_data) {
-		$try_block_count = [
-			"encountered but nothing caught" => 0,
-			"nothing encountered" => 0,
-			"caught and encountered" => 0
+		$try_blocks = [
+			"encountered but nothing caught" => [],
+			"nothing caught, unknown encountered" => [],
+			"nothing encountered" => [],
+			"caught and encountered" => []
 		];
 
 		foreach ($scope_data["guarded scopes"] as $guarded_scope_name => $guarded_scope_data) {
 			$inclosed_scope_name_arr = array_keys($guarded_scope_data["inclosed"]);
 			$inclosed_scope_name = array_pop($inclosed_scope_name_arr); //there can only be one;
-			$scope_encounters = $this->scopeEncountersAnException($guarded_scope_data["inclosed"][$inclosed_scope_name]);
+			$scope_encounters_any = $this->scopeEncountersAnException($guarded_scope_data["inclosed"][$inclosed_scope_name]);
 			$catch_clauses_catch_something = $this->catchesAnyExceptions($guarded_scope_data);
 
-			if ($scope_encounters === true && $catch_clauses_catch_something === false) {
-				$try_block_count["encountered but nothing caught"] += 1;
-			} else if ($scope_encounters === false) {
-				$try_block_count["nothing encountered"] += 1;
+			if ($scope_encounters_any === true && $catch_clauses_catch_something === false) {
+				$encounters = $this->getEncountersForScope($guarded_scope_data["inclosed"][$inclosed_scope_name]);
+				$encounters_unknown = false;
+				foreach ($encounters as $encountered_exception) {
+					if ($encountered_exception === "" || $encountered_exception === "unknown") {
+						$encounters_unknown = true;
+						break;
+					}
+				}
+				if ($encounters_unknown === true) {
+					$try_blocks["nothing caught, unknown encountered"][] = $guarded_scope_name;
+				} else {
+					$try_blocks["encountered but nothing caught"][] = $guarded_scope_name;
+				}
+			} else if ($scope_encounters_any === false) {
+				$try_blocks["nothing encountered"][] = $guarded_scope_name;
 			} else {
-				$try_block_count["caught and encountered"] += 1;
+				$try_blocks["caught and encountered"][] = $guarded_scope_name;
 			}
 
-			$nested_scope_accumulated_counts = $this->analyseScope($guarded_scope_data["inclosed"][$inclosed_scope_name]);
+			$nested_scope_accumulated = $this->analyseScope($guarded_scope_data["inclosed"][$inclosed_scope_name]);
 
-			$try_block_count["encountered but nothing caught"] += $nested_scope_accumulated_counts["encountered but nothing caught"];
-			$try_block_count["nothing encountered"] += $nested_scope_accumulated_counts["nothing encountered"];
-			$try_block_count["caught and encountered"] += $nested_scope_accumulated_counts["caught and encountered"];
+			$try_blocks["encountered but nothing caught"] = array_merge($try_blocks["encountered but nothing caught"], $nested_scope_accumulated["encountered but nothing caught"]);
+			$try_blocks["nothing caught, unknown encountered"] = array_merge($try_blocks["nothing caught, unknown encountered"], $nested_scope_accumulated["nothing caught, unknown encountered"]);
+			$try_blocks["nothing encountered"] = array_merge($try_blocks["nothing encountered"], $nested_scope_accumulated["nothing encountered"]);
+			$try_blocks["caught and encountered"] = array_merge($try_blocks["caught and encountered"], $nested_scope_accumulated["caught and encountered"]);
 		}
 
-		return $try_block_count;
+		return $try_blocks;
 	}
 
 	private function catchesAnyExceptions($guarded_scope_data) {
@@ -95,4 +117,14 @@ class AnalyseObsoleteTryBlocksCommand extends Command {
 		return empty($scope["raises"]) === false || empty($scope["propagates"]) === false || empty($scope["uncaught"]) === false;
 	}
 
+	private function getEncountersForScope($scope) {
+		$encounters = $scope["raises"];
+		foreach ($scope["propagates"] as $propagated_exceptions) {
+			$encounters = array_merge($propagated_exceptions, $encounters);
+		}
+		foreach ($scope["uncaught"] as $uncaught_exceptions) {
+			$encounters = array_merge($uncaught_exceptions, $encounters);
+		}
+		return $encounters;
+	}
 }
